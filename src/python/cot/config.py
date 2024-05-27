1c6b6331018ca96bd055811b050e988ff7591e97
Abreham Atlaw <abreham.atlaw@yahoo.com>
2024-05-27 10:54:40 +0300

    Production Changes

--- src/python/cot/config.py
+++ src/python/cot/config.py
@@ -8,9 +8,10 @@ DEPLOY_DIR = os.path.join(BUILD_DIR, "deploy/")
 SOLIDITY_DIR = os.path.join(BUILD_DIR, "solidity/")
 
 
-KEYSTORE_PATH = os.path.join(BASE_DIR, "data/node1/keystore/UTC--2024-05-18T10-36-01.139133873Z--750a6c1a06ef0397c7585f5e64550d7778465453")
+KEYSTORE_PATH = os.path.join(BASE_DIR, "data/node.remote/keystore/UTC--2024-05-24T06-59-28.035560526Z--95bb2e1af9f91713d6788bea51186dd4c5de882e")
 KEYSTORE_PASSWORD = "temppasswd" # TODO:MOVE THIS TO ENVIRNONMENT
 
 GAS_PRICE = 0
 
-NODE_URL = "http://127.0.0.1:8545"
+# NODE_URL = "http://127.0.0.1:8545"
+NODE_URL = "http://cot.alwaysdata.net"
